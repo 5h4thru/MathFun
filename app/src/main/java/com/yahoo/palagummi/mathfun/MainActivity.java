816b4f4652622c63b13d1cd6f903e5af32b68942
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-23 18:31:05 -0800

    Layout skeleton compelete

--- app/src/main/java/com/yahoo/palagummi/mathfun/MainActivity.java
+++ app/src/main/java/com/yahoo/palagummi/mathfun/MainActivity.java
@@ -2,12 +2,21 @@ package com.yahoo.palagummi.mathfun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton = null;
 
+    public void start(View view){
+        startButton.setVisibility(View.INVISIBLE);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the startButton
+        startButton = (Button) findViewById(R.id.startButton);
     }
 }
